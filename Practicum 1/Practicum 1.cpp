b31c27eba505850f2c79e1b1ee60ee457558a137
Jeroen Visser <j.visser10@student.avans.nl>
2022-05-26 13:05:52 +0200

    Chapter 1 stuff

---
+++ Practicum 1/Practicum 1.cpp
@@ -0,0 +1,47 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+void showImage()
+{
+	std::string path = "Resources/test.png";
+	cv::Mat img = cv::imread(path);
+	cv::imshow("Image", img);
+	cv::waitKey();
+}
+
+void showVideo() // Throws error at end of video file!
+{
+	std::string path = "Resources/test_video.mp4";
+	cv::VideoCapture capture(path);
+	cv::Mat img;
+
+	while (true)
+	{
+		capture.read(img);
+
+		cv::imshow("Image", img);
+		cv::waitKey(10);
+	}
+}
+
+void showWebcam()
+{
+	cv::VideoCapture capture(0);
+	cv::Mat img;
+
+	while (true)
+	{
+		capture.read(img);
+
+		cv::imshow("Image", img);
+		cv::waitKey(1);
+	}
+}
+
+int main()
+{
+	//showImage();
+	showVideo();
+	//showWebcam();
+}
